b1b1a4a564241c2498a7bd1678495022bc3eb98f
Pavel Novák <novak.pavel@ssakhk.cz>
2022-03-18 08:45:19 +0100

    update

--- Katastr/Controls/MapPanel.cs
+++ Katastr/Controls/MapPanel.cs
@@ -15,11 +15,17 @@ namespace Katastr.Controls
         private readonly List<Point> _points;
         public bool IsSelectMode { get; set; } = false;
         private const int POINT_SIZE = 4;
+        private const int LINE_SIZE = 7;
+        private readonly Brush _brush;
+        private Pen _pen;
+        private double _scale = 1;
         public MapPanel()
         {
             _map = new Bitmap(Width, Height);
             _points = new List<Point>();
             DoubleBuffered = true;
+            _brush = new SolidBrush(Color.FromArgb(20, 255, 0, 0));
+            _pen = new Pen(Color.Red, LINE_SIZE);
         }
         
         public void SetMap(Image map)
@@ -33,13 +39,24 @@ namespace Katastr.Controls
             base.OnPaint(e);
 
             var g = e.Graphics;
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            g.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
             //TODO: scale image
             g.DrawImage(_map, 0, 0);
-
+            if (_points.Count > 2)
+            {
+                var polygon = new List<Point>();
+                polygon.AddRange(_points);
+                polygon.Add(_points[0]);
+                g.DrawLines(Pens.Red, polygon.ToArray());
+                g.FillPolygon(_brush, polygon.ToArray());
+            }
             for (int i = 0; i < _points.Count; i++)
             {
                 var point = _points[i];
-                g.FillEllipse(Brushes.Red, point.X, point.Y, POINT_SIZE, POINT_SIZE);
+                int pointCenter = POINT_SIZE / 2;
+                g.FillEllipse(Brushes.Red, point.X - pointCenter, point.Y - pointCenter, POINT_SIZE, POINT_SIZE);
             }
         }
 
---
+++ Katastr/Utils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Katastr
+{
+    public static class Utils
+    {
+        public static double Distance(Point a, Point b)
+        {
+            return Math.Sqrt(Math.Pow(b.X - a.X, 2) + Math.Pow(b.Y - a.Y, 2));
+        }
+    }
+}
